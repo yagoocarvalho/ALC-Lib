2359483eba1f8f832220d0dd546f3e31e209a576
Eduardo <eduardodsaraujo@hotmail.com.br>
2017-06-03 13:16:06 -0300

    Add Inverse Interpolation Method

--- ALC_Lib/Lista4/Program.cs
+++ ALC_Lib/Lista4/Program.cs
@@ -14,12 +14,12 @@ namespace Lista4
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
-        private static double _Function (double x)
+        private static double _Function(double x)
         {
             double result = 0.0;
-            string functionString = ConfigurationManager.AppSettings.Get ("function");
+            string functionString = ConfigurationManager.AppSettings.Get("function");
             // TODO: CHANGE THIS TO THE FUNCTION WANTED FUNCTION
-            result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);       // Slide's example
+            result = Math.Pow(x, 2.0) - 4 * Math.Cos(x);       // Slide's example
             //result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);
 
             return result;
@@ -30,10 +30,10 @@ namespace Lista4
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
-        private static double _FunctionDerivative (double x)
+        private static double _FunctionDerivative(double x)
         {
             double result = 0.0;
-            string functionString = ConfigurationManager.AppSettings.Get ("function");
+            string functionString = ConfigurationManager.AppSettings.Get("function");
             // TODO: CHANGE THIS TO THE FUNCTION WANTED FUNCTION
             result = (2 * x) + (4 * Math.Sin(x));       // Slide's example
             //result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);
@@ -43,51 +43,56 @@ namespace Lista4
 
         private static double _result;
 
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine ("Start List 4 Execution");
+            Console.WriteLine("Start List 4 Execution");
 
-            string stringTol = ConfigurationManager.AppSettings.Get ("tolerance");
-            string stringDX  = ConfigurationManager.AppSettings.Get ("dx");
+            string stringTol = ConfigurationManager.AppSettings.Get("tolerance");
+            string stringDX = ConfigurationManager.AppSettings.Get("dx");
 
-            double tol    = Convert.ToDouble(stringTol.Split('^').First().Split('*').First()) * 
-                            Math.Pow (Convert.ToDouble(stringTol.Split('^').First().Split('*').Last()), 
+            double tol = Convert.ToDouble(stringTol.Split('^').First().Split('*').First()) *
+                            Math.Pow(Convert.ToDouble(stringTol.Split('^').First().Split('*').Last()),
                                       Convert.ToDouble(stringTol.Split('^').Last())
                             );
-            double dx     = Convert.ToDouble (stringDX.Split ('^').First ().Split ('*').First ()) *
-                            Math.Pow (Convert.ToDouble (stringDX.Split ('^').First ().Split ('*').Last ()),
-                                      Convert.ToDouble (stringDX.Split ('^').Last ())
+            double dx = Convert.ToDouble(stringDX.Split('^').First().Split('*').First()) *
+                            Math.Pow(Convert.ToDouble(stringDX.Split('^').First().Split('*').Last()),
+                                      Convert.ToDouble(stringDX.Split('^').Last())
                             );
-            double a      = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("a"));
-            double b      = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("b"));
-            double x0     = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("x0"));
-            int    nIter  = Convert.ToInt32  (ConfigurationManager.AppSettings.Get ("nIter"));
+            double a = Convert.ToDouble(ConfigurationManager.AppSettings.Get("a"));
+            double b = Convert.ToDouble(ConfigurationManager.AppSettings.Get("b"));
+            double c = Convert.ToDouble(ConfigurationManager.AppSettings.Get("c"));
+            double x0 = Convert.ToDouble(ConfigurationManager.AppSettings.Get("x0"));
+            int nIter = Convert.ToInt32(ConfigurationManager.AppSettings.Get("nIter"));
 
 
-            string method = ConfigurationManager.AppSettings.Get ("method").ToLower();
+            string method = ConfigurationManager.AppSettings.Get("method").ToLower();
 
             switch (method)
             {
                 case "bissection":
-                    _result = SolveByBissection (tol, a, b);
+                    _result = SolveByBissection(tol, a, b);
                     break;
                 case "newton-original":
-                    _result = SolveByNewtonOriginal (tol, x0, nIter);
+                    _result = SolveByNewtonOriginal(tol, x0, nIter);
                     break;
                 case "newton-secant":
-                    _result = SolveByNewtonSecant (tol, x0, nIter, dx);
+                    _result = SolveByNewtonSecant(tol, x0, nIter, dx);
+                    break;
+                case "inverse-interpolation":
+                    _result = SolveByInverseInterpolation(tol, a, b, c, nIter);
                     break;
                 default:
-                    Console.WriteLine ("Wrong solving method");
-                    System.Environment.Exit (-100);
+                    Console.WriteLine("Wrong solving method");
+                    System.Environment.Exit(-100);
                     break;
             }
 
             if (_result == -1)
             {
-                System.Environment.Exit (-101);
+                System.Environment.Exit(-101);
             }
-            Console.WriteLine ("Final Result = " + _result);
+            Console.WriteLine("Final Result = " + _result);
+            Console.ReadLine();
 
         }
 
@@ -102,9 +107,9 @@ namespace Lista4
             {
                 count++;
                 x = (a + b) / 2.0;
-                f = _Function (x);
+                f = _Function(x);
 
-                Console.WriteLine (String.Format ("Iter:{0}\ta:{1}\tb:{2}\t:{3}\tf:{4}\t|b-a|:{5} ", count, a, b, x, f, Math.Abs (b - a)));
+                Console.WriteLine(String.Format("Iter:{0}\ta:{1}\tb:{2}\t:{3}\tf:{4}\t|b-a|:{5} ", count, a, b, x, f, Math.Abs(b - a)));
 
                 if (f > 0.0)
                 {
@@ -121,23 +126,23 @@ namespace Lista4
             return result;
         }
 
-        public static double SolveByNewtonOriginal (double tol, double x0, double nIter)
+        public static double SolveByNewtonOriginal(double tol, double x0, double nIter)
         {
-            double       result  = 0.0;
-            double       tolk    = 0.0;
-            List<double> x       = new List<double>();
-            x.Add (x0);
+            double result = 0.0;
+            double tolk = 0.0;
+            List<double> x = new List<double>();
+            x.Add(x0);
 
             for (int k = 1; k <= nIter; k++)
             {
-                x.Add (x[k-1] - ( _Function (x[k-1]) / _FunctionDerivative (x[k-1]) ) );
-                tolk = Math.Abs (x[k] - x[k-1]);
-                Console.WriteLine (String.Format ("Iter:{0}\tx[k-1]:{1}\tf(x[k-1]):{2}\tf'(x[k-1]):{3}\tx[k]:{4}\ttolk:{5} ", 
-                                                    k, 
+                x.Add(x[k - 1] - (_Function(x[k - 1]) / _FunctionDerivative(x[k - 1])));
+                tolk = Math.Abs(x[k] - x[k - 1]);
+                Console.WriteLine(String.Format("Iter:{0}\tx[k-1]:{1}\tf(x[k-1]):{2}\tf'(x[k-1]):{3}\tx[k]:{4}\ttolk:{5} ",
+                                                    k,
                                                     x[k - 1].ToString("0.000"),
-                                                    _Function (x[k - 1]).ToString ("0.000"),
-                                                    _FunctionDerivative (x[k - 1]).ToString ("0.000"),
-                                                    x[k].ToString ("0.000"), 
+                                                    _Function(x[k - 1]).ToString("0.000"),
+                                                    _FunctionDerivative(x[k - 1]).ToString("0.000"),
+                                                    x[k].ToString("0.000"),
                                                     tolk));
                 if (tolk < tol)
                 {
@@ -149,27 +154,27 @@ namespace Lista4
             return -1;
         }
 
-        public static double SolveByNewtonSecant (double tol, double x0, double nIter, double dx)
+        public static double SolveByNewtonSecant(double tol, double x0, double nIter, double dx)
         {
-            double       result  = 0.0;
-            double       tolk    = 0.0;
-            List<double> x       = new List<double> ();
-            x.Add (x0);
-            x.Add (x0 + dx);
+            double result = 0.0;
+            double tolk = 0.0;
+            List<double> x = new List<double>();
+            x.Add(x0);
+            x.Add(x0 + dx);
 
-            double fa = _Function (x0);
+            double fa = _Function(x0);
             double fi = 0.0;
 
             for (int k = 1; k <= nIter; k++)
             {
-                fi = _Function (x[k]);
-                x.Add (x[k] - (fi * (x[k] - x[k-1]) / (fi - fa)));
-                tolk = Math.Abs (x[k+1] - x[k]);
-                Console.WriteLine (String.Format ("Iter:{0}\tx[k-1]:{1}\tx[k]:{2}\tx[k+1]:{3}\ttolk:{4}",
+                fi = _Function(x[k]);
+                x.Add(x[k] - (fi * (x[k] - x[k - 1]) / (fi - fa)));
+                tolk = Math.Abs(x[k + 1] - x[k]);
+                Console.WriteLine(String.Format("Iter:{0}\tx[k-1]:{1}\tx[k]:{2}\tx[k+1]:{3}\ttolk:{4}",
                                                     k,
-                                                    x[k - 1].ToString ("0.000"),
-                                                    x[k].ToString ("0.000"),
-                                                    x[k + 1].ToString ("0.000"),
+                                                    x[k - 1].ToString("0.000"),
+                                                    x[k].ToString("0.000"),
+                                                    x[k + 1].ToString("0.000"),
                                                     tolk));
                 if (tolk < tol)
                 {
@@ -184,5 +189,63 @@ namespace Lista4
 
             return -1;
         }
+        public static double SolveByInverseInterpolation(double tol, double a, double b, double c, double nIter)
+        {
+            double result = 0.0;
+            double tolk = 0.0;
+            double[] x = { a, b, c };
+            double[] y = new double[3];
+
+            List<double> xk = new List<double>();
+            xk.Add(Math.Pow(10, 36));
+
+            double xk_now;
+            for (int k = 1; k <= nIter; k++)
+            {
+                y[0] = _Function(x[0]);
+                y[1] = _Function(x[1]);
+                y[2] = _Function(x[2]);
+
+                xk_now = ((y[1] * y[2] * x[0]) / ((y[0] - y[1]) * (y[0] - y[2]))) +
+                    ((y[0] * y[2] * x[1]) / ((y[1] - y[0]) * (y[1] - y[2]))) +
+                    ((y[0] * y[1] * x[2]) / ((y[2] - y[0]) * (y[2] - y[1])));
+
+                xk.Add(xk_now);
+                tolk = Math.Abs(xk[k] - xk[k - 1]);
+                Console.WriteLine(String.Format("Iter:{0}\tx1:{1}\tx2:{2}\tx3:{3}\ty1:{4}\ty2:{5}\ty3:{6}\txk:{7}\ttol(k):{8}",
+                                                    k,
+                                                    x[0].ToString("0.000"),
+                                                    x[1].ToString("0.000"),
+                                                    x[2].ToString("0.000"),
+                                                    y[0].ToString("0.000"),
+                                                    y[1].ToString("0.000"),
+                                                    y[2].ToString("0.000"),
+                                                    xk[k].ToString("0.000"),
+                                                    tolk));
+                if (tolk < tol)
+                {
+                    result = xk[k];
+                    return result;
+                }
+                else
+                {
+                    double max = y.Max(element => Math.Abs(element));
+                    for (int i = 0; i < y.Count(); i++)
+                    {
+                        if (max == Math.Abs(y[i]))
+                        {
+                            x[i] = xk[k];
+                            break;
+                        }
+                    }
+                    Array.Sort(x);
+                }
+            }
+
+            return -1;
+
+        }
+
+
     }
 }
