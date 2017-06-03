251c1efcfc4e83f9abf817e4467b2b38cf1c28db
Yago Carvalho <yago.meira@poli.ufrj.br>
2017-06-03 02:05:40 -0300

    Add Newton Methods (Original and Secant)

--- ALC_Lib/Lista4/Program.cs
+++ ALC_Lib/Lista4/Program.cs
@@ -9,25 +9,61 @@ namespace Lista4
 {
     class Program
     {
+        /// <summary>
+        /// Method that calculates the function to x
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         private static double _Function (double x)
         {
             double result = 0.0;
+            string functionString = ConfigurationManager.AppSettings.Get ("function");
+            // TODO: CHANGE THIS TO THE FUNCTION WANTED FUNCTION
+            result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);       // Slide's example
+            //result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);
 
-            result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);
+            return result;
+        }
+
+        /// <summary>
+        /// Method that calculates the function to x
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private static double _FunctionDerivative (double x)
+        {
+            double result = 0.0;
+            string functionString = ConfigurationManager.AppSettings.Get ("function");
+            // TODO: CHANGE THIS TO THE FUNCTION WANTED FUNCTION
+            result = (2 * x) + (4 * Math.Sin(x));       // Slide's example
+            //result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);
 
             return result;
         }
 
         private static double _result;
+
         static void Main (string[] args)
         {
             Console.WriteLine ("Start List 4 Execution");
 
             string stringTol = ConfigurationManager.AppSettings.Get ("tolerance");
+            string stringDX  = ConfigurationManager.AppSettings.Get ("dx");
 
-            double tol    = Math.Pow (Convert.ToDouble(stringTol.Split('^').First()), Convert.ToDouble(stringTol.Split('^').Last()));
+            double tol    = Convert.ToDouble(stringTol.Split('^').First().Split('*').First()) * 
+                            Math.Pow (Convert.ToDouble(stringTol.Split('^').First().Split('*').Last()), 
+                                      Convert.ToDouble(stringTol.Split('^').Last())
+                            );
+            double dx     = Convert.ToDouble (stringDX.Split ('^').First ().Split ('*').First ()) *
+                            Math.Pow (Convert.ToDouble (stringDX.Split ('^').First ().Split ('*').Last ()),
+                                      Convert.ToDouble (stringDX.Split ('^').Last ())
+                            );
             double a      = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("a"));
             double b      = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("b"));
+            double x0     = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("x0"));
+            int    nIter  = Convert.ToInt32  (ConfigurationManager.AppSettings.Get ("nIter"));
+
+
             string method = ConfigurationManager.AppSettings.Get ("method").ToLower();
 
             switch (method)
@@ -35,7 +71,11 @@ namespace Lista4
                 case "bissection":
                     _result = SolveByBissection (tol, a, b);
                     break;
-                case "newton":
+                case "newton-original":
+                    _result = SolveByNewtonOriginal (tol, x0, nIter);
+                    break;
+                case "newton-secant":
+                    _result = SolveByNewtonSecant (tol, x0, nIter, dx);
                     break;
                 default:
                     Console.WriteLine ("Wrong solving method");
@@ -43,6 +83,10 @@ namespace Lista4
                     break;
             }
 
+            if (_result == -1)
+            {
+                System.Environment.Exit (-101);
+            }
             Console.WriteLine ("Final Result = " + _result);
 
         }
@@ -75,6 +119,70 @@ namespace Lista4
             result = x;
 
             return result;
+        }
+
+        public static double SolveByNewtonOriginal (double tol, double x0, double nIter)
+        {
+            double       result  = 0.0;
+            double       tolk    = 0.0;
+            List<double> x       = new List<double>();
+            x.Add (x0);
+
+            for (int k = 1; k <= nIter; k++)
+            {
+                x.Add (x[k-1] - ( _Function (x[k-1]) / _FunctionDerivative (x[k-1]) ) );
+                tolk = Math.Abs (x[k] - x[k-1]);
+                Console.WriteLine (String.Format ("Iter:{0}\tx[k-1]:{1}\tf(x[k-1]):{2}\tf'(x[k-1]):{3}\tx[k]:{4}\ttolk:{5} ", 
+                                                    k, 
+                                                    x[k - 1].ToString("0.000"),
+                                                    _Function (x[k - 1]).ToString ("0.000"),
+                                                    _FunctionDerivative (x[k - 1]).ToString ("0.000"),
+                                                    x[k].ToString ("0.000"), 
+                                                    tolk));
+                if (tolk < tol)
+                {
+                    result = x[k];
+                    return result;
+                }
+            }
+
+            return -1;
+        }
+
+        public static double SolveByNewtonSecant (double tol, double x0, double nIter, double dx)
+        {
+            double       result  = 0.0;
+            double       tolk    = 0.0;
+            List<double> x       = new List<double> ();
+            x.Add (x0);
+            x.Add (x0 + dx);
+
+            double fa = _Function (x0);
+            double fi = 0.0;
+
+            for (int k = 1; k <= nIter; k++)
+            {
+                fi = _Function (x[k]);
+                x.Add (x[k] - (fi * (x[k] - x[k-1]) / (fi - fa)));
+                tolk = Math.Abs (x[k+1] - x[k]);
+                Console.WriteLine (String.Format ("Iter:{0}\tx[k-1]:{1}\tx[k]:{2}\tx[k+1]:{3}\ttolk:{4}",
+                                                    k,
+                                                    x[k - 1].ToString ("0.000"),
+                                                    x[k].ToString ("0.000"),
+                                                    x[k + 1].ToString ("0.000"),
+                                                    tolk));
+                if (tolk < tol)
+                {
+                    result = x[k];
+                    return result;
+                }
+                else
+                {
+                    fa = fi;
+                }
+            }
+
+            return -1;
         }
     }
 }
