f4c901d115b80cf7ea3380374481c214fb3a2780
Yago Carvalho <yago.meira@poli.ufrj.br>
2017-06-04 01:09:57 -0300

    Finished Lista 5

---
+++ ALC_Lib/Lista5/IntegrationRules.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista5
+{
+    public class IntegrationRules
+    {
+        public delegate double Function (double x);
+
+        public static double MidPointRule (Function f, double a, double b)
+        {
+            double m = (b + a) / 2.0;
+            double L = (b - a);
+
+            return f (m) * L;
+        }
+
+        public static double TrapeziumRule (Function f, double a, double b)
+        {
+            double L = (b - a);
+            return ((f(a) + f(b)) / 2.0) * L;
+        }
+
+        public static double SimpsonRule (Function f, double a, double b)
+        {
+            double m = (b + a) / 2.0;
+            double L = (b - a);
+
+            return ((f (a) + 4 * f (m) + f (b))) * (L / 6.0);
+        }
+
+        public static double SolveByMT (Function f, double a, double b)
+        {
+            Console.WriteLine ("Using MidPoint and Trapezium");
+            double Mf = MidPointRule  (f, a, b);
+            double Tf = TrapeziumRule (f, a, b);
+            double Ef = (Tf - Mf) / 3.0;
+            Console.WriteLine ("Mf = " + Mf);
+            Console.WriteLine ("Tf = " + Tf);
+            Console.WriteLine ("Ef = " + Ef);
+
+            return Mf + Ef;
+        }
+
+        public static double SolveByMTS (Function f, double a, double b)
+        {
+            Console.WriteLine ("Using Simpson");
+            double Mf = MidPointRule  (f, a, b);
+            double Tf = TrapeziumRule (f, a, b);
+            double Sf = SimpsonRule   (f, a, b);
+            double Ef = (Tf - Mf) / 3.0;
+            double Ff = (Tf - Mf - 3 * Ef) / 5.0;
+            Console.WriteLine ("Mf = " + Mf);
+            Console.WriteLine ("Tf = " + Tf);
+            Console.WriteLine ("Sf = " + Sf);
+            Console.WriteLine ("Ef = " + Ef);
+            Console.WriteLine ("Ff = " + Ff);
+
+            return Sf - (2*Ff/3);
+        }
+    }
+}
--- ALC_Lib/Lista5/Program.cs
+++ ALC_Lib/Lista5/Program.cs
@@ -11,41 +11,15 @@ namespace Lista5
 {
     class Program
     {
-        /// <summary>
-        /// Method that calculates the function to x
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        private static double _Function (double x)
-        {
-            return Math.Exp (-1 * Math.Pow (x, 2.0));
-        }
-
-        /// <summary>
-        /// Method that calculates the function to x
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        private static double _FunctionDerivative (double x)
-        {
-            double result = 0.0;
-            string functionString = ConfigurationManager.AppSettings.Get ("function");
-            // TODO: CHANGE THIS TO THE FUNCTION WANTED FUNCTION
-            result = (2 * x) + (4 * Math.Sin (x));       // Slide's example
-            //result = Math.Pow (x, 2.0) - 4 * Math.Cos (x);
-
-            return result;
-        }
-
         private static double _result;
 
         static void Main (string[] args)
         {
-            Console.WriteLine ("Start List 4 Execution");
+            Console.WriteLine ("Start List 5 Execution");
 
             // Reading configs
-            int     nIntegrationPoints       = Convert.ToInt32 (ConfigurationManager.AppSettings.Get ("nIntegrationPoints"));
-            string  method                   = ConfigurationManager.AppSettings.Get ("method").ToLower ();
+            int     nIntegrationPoints      = Convert.ToInt32 (ConfigurationManager.AppSettings.Get ("nIntegrationPoints"));
+            string  method                  = ConfigurationManager.AppSettings.Get ("method").ToLower ();
             double inferiorIntegrationLimit = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("inferiorIntegrationLimit"));
             double superiorIntegrationLimit = Convert.ToDouble (ConfigurationManager.AppSettings.Get ("superiorIntegrationLimit"));
             
@@ -67,9 +41,28 @@ namespace Lista5
                     Console.Write ("Press any key to continue...");
                     Console.ReadLine ();
                     break;
-                case "gauss-quadrature":
+                case "gaussian-quadrature":
                     _result = GaussianQuadrature.Solve (_Function, inferiorIntegrationLimit, superiorIntegrationLimit, nIntegrationPoints);
                     Console.WriteLine ("Final Result = " + _result);
+                    Console.Write ("Press any key to continue...");
+                    Console.ReadLine ();
+                    break;
+                case "rules":
+                    _result = IntegrationRules.SolveByMT (_Function, inferiorIntegrationLimit, superiorIntegrationLimit);
+                    Console.WriteLine ("A(f) = " + _result);
+                    Console.Write ("Press any key to continue...");
+                    Console.ReadLine ();
+                    _result = IntegrationRules.SolveByMTS (_Function, inferiorIntegrationLimit, superiorIntegrationLimit);
+                    Console.WriteLine ("A(f) = " + _result);
+                    Console.Write ("Press any key to continue...");
+                    Console.ReadLine ();
+                    _result = GaussianQuadrature.Solve (_Function, inferiorIntegrationLimit, superiorIntegrationLimit, nIntegrationPoints);
+                    Console.WriteLine ("Using Gauss = " + _result);
+                    Console.Write ("Press any key to continue...");
+                    Console.ReadLine ();
+                    _result = PolinomialIntegration.Solve (_Function, inferiorIntegrationLimit, superiorIntegrationLimit, nIntegrationPoints);
+                    Console.WriteLine ("Using Polinomial = " + _result);
+                    Console.WriteLine ("Finished!");
                     Console.Write ("Press any key to continue...");
                     Console.ReadLine ();
                     break;
@@ -83,10 +76,59 @@ namespace Lista5
 
         }
 
-        private static double SolveByGaussQuadrature ()
+        /// <summary>
+        /// Method that calculates the function to x
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private static double _Function (double x)
         {
-            throw new NotImplementedException ();
+            // Slide's example
+            //return (2.0 + x + (2 * Math.Pow (x, 2.0))); 
+
+            // Question 2
+            //return (1.0 / Math.Sqrt (2 * Math.PI)) * Math.Exp ((-1.0 / 2.0) * Math.Pow (x, 2.0));
+
+            // Questions 3 and 4
+            //double w = x;
+            //double wn = 1.0;
+            //double e = 0.05;
+
+            // m0
+            //return Sq(w, wn, e);
+
+            //m2
+            //return Math.Pow (w, 2.0) * Sq (w, wn, e);
+
+            // Question 5
+            //return 2.0 + 2.0 * x - Math.Pow (x, 2.0) + 3 * Math.Pow (x, 3);
+
+            // Question 6
+            return (1.0) / (1.0 + Math.Pow (x, 2.0));
         }
-        
+
+        private static double Sn (double w)
+        {
+            // Question 3
+            //return 2.0;
+
+            // Question 4
+            double Hs = 3.0;
+            double Tz = 5.0;
+
+            return ((4.0 * Math.Pow (Math.PI, 3.0) * Math.Pow (Hs, 2.0)) / (Math.Pow (w, 5.0) * Math.Pow (Tz, 4.0))) * Math.Exp ((-16.0 * Math.Pow (Math.PI, 3.0)) / (Math.Pow (w, 4.0) * Math.Pow (Tz, 4.0)));
+        }
+
+        private static double Sq (double w, double wn, double e)
+        {
+            // Question 3 and 4
+            return Math.Pow (RAO (w, wn, e), 2.0) * Sn(w);
+        }
+
+        private static double RAO (double w, double wn, double e)
+        {
+            // Questions 3 and 4
+            return 1.0 / Math.Sqrt (Math.Pow (1.0 - Math.Pow ((w / wn), 2.0), 2.0) + Math.Pow (2.0 * e * (w / wn), 2.0));
+        }
     }
 }
